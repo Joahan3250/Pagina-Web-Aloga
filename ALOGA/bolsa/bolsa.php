f4895de845586b4dc1c3b918b07e5f7841d487ae
Joahan3250 <147442724+Joahan3250@users.noreply.github.com>
2024-12-17 10:21:52 -0600

    Add files via upload

---
+++ ALOGA/bolsa/bolsa.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="LOG_PEST_Blanco.png" type="image/png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link href="bolsa.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <title>Bolsa de trabajo </title>
+</head>
+<body>
+
+
+
+
+<section>
+
+<?php  include('section_bolsa.php')?>
+</section>
+  
+     
+
+</body>
+</html>
